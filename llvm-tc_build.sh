840b4d6dec3cfdf1db20377171c36a80095257a0
ben863 <r.budhiono@gmail.com>
2021-07-05 01:48:52 +0700

    Update llvm-tc_build.sh
    change branch source

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use tcbuild build script as LLVM Build Script.
-git clone https://github.com/cbendot/tcbuild $(pwd)/llvm-tc -b tcbuild-gcc   
+git clone https://github.com/cbendot/tcbuild $(pwd)/llvm-tc    
 cd $(pwd)/llvm-tc
 bash build-tc.sh
