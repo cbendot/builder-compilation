01dd76d6addf2cf8536cc7c159d25eccfb51283e
ben863 <r.budhiono@gmail.com>
2021-07-05 08:47:39 +0700

    revert: to old source branch

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use tcbuild build script as LLVM Build Script.
-git clone https://github.com/cbendot/tcbuild $(pwd)/llvm-tc    
+git clone https://github.com/cbendot/tcbuild $(pwd)/llvm-tc -b llvm-tc_template    
 cd $(pwd)/llvm-tc
 bash build-tc.sh
