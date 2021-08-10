0d8ef160f2358df36b4f877c83c9660e54fba151
ben863 <r.budhiono@gmail.com>
2021-08-10 20:47:48 +0700

    Update kernelbuild.sh

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -71,16 +71,11 @@ tg_post_msg "<b>xKernelCompiler:</b><code>Compilation has started"
 cd ${KERNEL_ROOTDIR}
 make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
-    CC=${CLANG_ROOTDIR}/bin/clang \
-    HOSTCC=${CLANG_ROOTDIR}/bin/clang \
-    HOSTCXX=${CLANG_ROOTDIR}/bin/clang++ \
-    AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-        AS=${CLANG_ROOTDIR}/bin/llvm-as \
-  	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-        OBJSIZE=${CLANG_ROOTDIR}/bin/llvm-size \
+    CC=${CLANG_ROOTDIR}/bin/clang \    
+    AR=${CLANG_ROOTDIR}/bin/llvm-ar \        
+  	NM=${CLANG_ROOTDIR}/bin/llvm-nm \        
   	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
-  	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
-    READELF=${CLANG_ROOTDIR}/bin/llvm-readelf \
+  	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \    
     STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
