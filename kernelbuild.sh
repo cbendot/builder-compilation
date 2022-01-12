845c67984a666f1bbdbcd863e25e746d9041085c
ben863 <r.budhiono@gmail.com>
2022-01-12 12:47:07 +0700

    stock upstream

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -112,7 +112,7 @@ function finerr() {
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 [LV][OC]$KERNEL_NAME-HMP-${ZIP_DATE}.zip *
+    zip -r9 [LV]$KERNEL_NAME-HMP-${ZIP_DATE}.zip *
     cd ..
 
 }
