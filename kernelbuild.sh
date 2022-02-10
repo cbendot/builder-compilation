561ef75e9fead8197b97ce411132dceae190ba9b
ben863 <r.budhiono@gmail.com>
2022-02-11 02:29:05 +0700

    add again llvm-objcopy

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -73,7 +73,8 @@ make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
 make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
     AR=${CLANG_ROOTDIR}/bin/llvm-ar \
-  	NM=${CLANG_ROOTDIR}/bin/llvm-nm \  	
+  	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
+        OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \  	
   	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
     STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
