33b67be2553d2fafb098b24d9099cda589798d62
ben863 <r.budhiono@gmail.com>
2022-02-12 23:08:49 +0700

    import script from ci

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -74,8 +74,8 @@ make -j$(nproc) ARCH=arm64 O=out \
     CC=${CLANG_ROOTDIR}/bin/clang \
     AR=${CLANG_ROOTDIR}/bin/llvm-ar \
   	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
-  	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \  	
-        OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
+  	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
+  	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
     STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
     CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
