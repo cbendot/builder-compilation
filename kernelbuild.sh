0898d051c5105b17b8636a802f92ff4951a33447
ben863 <r.budhiono@gmail.com>
2022-06-18 19:40:08 +0700

    fix export gcc version
    Change-Id: Ib93858bce84827af32b35e57d8ccdaa0fa4c549e
    Signed-off-by: ben863 <r.budhiono@gmail.com>

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -36,7 +36,7 @@ export KBUILD_BUILD_HOST=$BUILD_HOST # Change with your own hostname.
 # Main Declaration
 CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
 LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
-GCC_VER="$("$GCC32_ROOTDIR"/bin/aarch64-buildroot-linux-gnu-gcc --version | head -n 1)"
+GCC_VER="$("$GCC64_ROOTDIR"/bin/aarch64-buildroot-linux-gnu-gcc --version | head -n 1)"
 export KBUILD_COMPILER_STRING="$CLANG_VER $LLD_VER $GCC_VER"
 COMMIT_HEAD=$(git log --oneline -1)
 IMAGE=$(pwd)/$DEVICE_CODENAME/out/arch/arm64/boot/Image.gz-dtb
