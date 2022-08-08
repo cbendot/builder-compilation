9aafce18e2aa87236382074b1cd39018ffd34ae6
ben863 <r.budhiono@gmail.com>
2022-08-08 13:11:56 +0700

    update clang

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -19,10 +19,13 @@ START=$(date +"%s")
 echo "|| Downloading few Dependecies . . .||"
 # Kernel Sources
 git clone --depth=1 $KERNEL_SOURCE -b msm-4.4-eas $DEVICE_CODENAME
-git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r445002.git aosp-clang
-# git clone --depth=1 https://gitlab.com/ben863/aosp-clang.git aosp-clang 
-git clone --depth=1 https://github.com/cbendot/gcc-aarch64.git gcc64
-git clone --depth=1 https://github.com/cbendot/gcc-armv5.git gcc32
+# git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r445002.git aosp-clang
+# git clone --depth=1 https://gitlab.com/ben863/aosp-clang.git aosp-clang
+git clone --depth=1 https://gitlab.com/STRK-ND/aosp-clang.git aosp-clang
+git clone --depth=1 https://gitlab.com/STRK-ND/aarch64-linux-android-4.9.git gcc64
+git clone --depth=1 https://gitlab.com/STRK-ND/arm-linux-androideabi-4.9.git gcc32
+# git clone --depth=1 https://github.com/cbendot/gcc-aarch64.git gcc64
+# git clone --depth=1 https://github.com/cbendot/gcc-armv5.git gcc32
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
