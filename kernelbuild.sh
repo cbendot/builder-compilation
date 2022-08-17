20ee1e472a641ffb1c75eca1d3bfdbd4b4ff8433
ben863 <r.budhiono@gmail.com>
2022-08-17 09:25:07 +0700

    Change kernel source

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -18,7 +18,7 @@
 START=$(date +"%s")
 echo "|| Downloading few Dependecies . . .||"
 # Kernel Sources
-git clone --depth=1 https://github.com/LineageOS/android_kernel_xiaomi_sm6125.git -b lineage-18.1 ginkgo
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_ginkgo.git -b arrow-11.0 ginkgo
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r445002.git aosp-clang
 # git clone --depth=1 https://gitlab.com/ben863/aosp-clang.git aosp-clang
 # git clone --depth=1 https://gitlab.com/STRK-ND/aarch64-linux-android-4.9.git gcc64
