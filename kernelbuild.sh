46f304d03811b7eeb34d2c78f79e7f4c77dd8faa
ben863 <r.budhiono@gmail.com>
2022-09-02 21:11:12 +0700

    fix defconfig

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -22,16 +22,14 @@ git clone --depth=1 https://github.com/cbendot/android_kernel_xiaomi_ginkgo.git 
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r445002.git -b 12.1 aosp-clang
 # git clone --depth=1 https://gitlab.com/ben863/aosp-clang.git aosp-clang
 # git clone --depth=1 https://gitlab.com/ben863/azure-clang.git aosp-clang
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-19.1 gcc64
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-19.1 gcc32
-# git clone --depth=1 https://gitlab.com/STRK-ND/aarch64-linux-android-4.9.git gcc64
-# git clone --depth=1 https://gitlab.com/STRK-ND/arm-linux-androideabi-4.9.git gcc32
-# git clone --depth=1 https://github.com/cbendot/gcc-aarch64.git gcc64
-# git clone --depth=1 https://github.com/cbendot/gcc-armv5.git gcc32
+# git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-19.1 gcc64
+# git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-19.1 gcc32
+git clone --depth=1 https://github.com/cbendot/gcc-aarch64.git gcc64
+git clone --depth=1 https://github.com/cbendot/gcc-armv5.git gcc32
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/ginkgo # IMPORTANT ! Fill with your kernel source root directory.
-DEVICE_DEFCONFIG="vendor/ginkgo-perf_defconfig" # IMPORTANT ! Declare your kernel source defconfig file here.
+DEVICE_DEFCONFIG="ginkgo-perf_defconfig" # IMPORTANT ! Declare your kernel source defconfig file here.
 DEVICE_CODENAME="ginkgo"
 CLANG_ROOTDIR=$(pwd)/aosp-clang # IMPORTANT! Put your clang directory here.
 # LLVM_ROOTDIR=$(pwd)/clang-llvm
@@ -89,10 +87,13 @@ make -j$(nproc) ARCH=arm64 SUBARCH=arm64 O=out \
   	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
     LD=${CLANG_ROOTDIR}/bin/ld.lld \
     STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
-    CLANG_TRIPLE=${GCC64_ROOTDIR}/aarch64-linux-gnu- \
-    CROSS_COMPILE=${GCC64_ROOTDIR}/bin/aarch64-linux-android- \
-    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-linux-androideabi-
-
+#    CLANG_TRIPLE=${GCC64_ROOTDIR}/aarch64-linux-gnu- \
+#    CROSS_COMPILE=${GCC64_ROOTDIR}/bin/aarch64-linux-android- \
+#    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-linux-androideabi-
+    CLANG_TRIPLE=${GCC64_ROOTDIR}/aarch64-buildroot-linux-gnu- \
+    CROSS_COMPILE=${GCC64_ROOTDIR}/bin/aarch64-buildroot-linux-gnu- \
+    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-buildroot-linux-gnueabi-
+   
    if ! [ -a "$IMAGE" ]; then
 	finerr
 	exit 1
