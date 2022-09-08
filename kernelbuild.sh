d47f0c0958bebc2d17ef2968050b849be72305dd
ben863 <r.budhiono@gmail.com>
2022-09-08 16:36:47 +0700

    build with ld.lld

--- kernelbuild.sh
+++ kernelbuild.sh
@@ -21,10 +21,10 @@ git clone --depth=1 $KERNEL_SOURCE -b msm-4.4-eas $DEVICE_CODENAME
 # git clone --depth=1 https://gitlab.com/ben863/elastics-clang clang-aosp # Elastics set as Clang Default
 # git clone --depth=1 https://gitlab.com/ben863/aosp-clang.git clang-aosp
 git clone --depth=1 https://gitlab.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-r445002.git clang-aosp
-# git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-19.1 gcc64
-# git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-19.1 gcc32
-git clone --depth=1 https://github.com/cbendot/gcc-aarch64.git gcc64
-git clone --depth=1 https://github.com/cbendot/gcc-armv5.git gcc32 
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9.git -b lineage-19.1 gcc64
+git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9.git -b lineage-19.1 gcc32
+# git clone --depth=1 https://github.com/cbendot/gcc-aarch64.git gcc64
+# git clone --depth=1 https://github.com/cbendot/gcc-armv5.git gcc32 
 
 # Main Declaration
 KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
@@ -89,12 +89,13 @@ make -j$(nproc) ARCH=arm64 SUBARCH=arm64 O=out \
   	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
   	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
     STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
-    CLANG_TRIPLE=${GCC64_ROOTDIR}/aarch64-buildroot-linux-gnu- \
-    CROSS_COMPILE=${GCC64_ROOTDIR}/bin/aarch64-buildroot-linux-gnu- \
-    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-buildroot-linux-gnueabi-
-#    CLANG_TRIPLE=${GCC64_ROOTDIR}/aarch64-linux-gnu- \
-#    CROSS_COMPILE=${GCC64_ROOTDIR}/bin/aarch64-linux-android- \
-#    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-linux-androideabi-
+    LD=${CLANG_ROOTDIR}/bin/ld.lld \
+#    CLANG_TRIPLE=${GCC64_ROOTDIR}/aarch64-buildroot-linux-gnu- \
+#    CROSS_COMPILE=${GCC64_ROOTDIR}/bin/aarch64-buildroot-linux-gnu- \
+#    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-buildroot-linux-gnueabi-
+    CLANG_TRIPLE=${GCC64_ROOTDIR}/aarch64-linux-gnu- \
+    CROSS_COMPILE=${GCC64_ROOTDIR}/bin/aarch64-linux-android- \
+    CROSS_COMPILE_ARM32=${GCC32_ROOTDIR}/bin/arm-linux-androideabi-
 
    if ! [ -a "$IMAGE" ]; then
 	error
